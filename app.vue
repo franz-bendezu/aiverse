9f182336fb8a77b540fd0f37f90212fcb516aa57
Franz Bendezu <franzant20@gmail.com>
2023-06-03 05:57:14 -0500

    feat: setup cofnig for vue-dapp for connect

--- app.vue
+++ app.vue
@@ -1,5 +1,70 @@
+<script setup lang="ts">
+import {
+  MetaMaskConnector,
+  WalletConnectConnector,
+  CoinbaseWalletConnector,
+  SafeConnector,
+  Connector,
+} from "vue-dapp";
+import { ref, onBeforeMount } from "vue";
+
+const isDev = import.meta.env.DEV;
+const infuraId = isDev
+  ? import.meta.env.VITE_INFURA_KEY
+  : "ff6a249a74e048f1b413cba715f98d07";
+
+let connectors: Connector[] = [
+  new MetaMaskConnector({
+    appUrl: "http://localhost:3000",
+  }),
+  new WalletConnectConnector({
+    qrcode: true,
+    rpc: {
+      1: `https://mainnet.infura.io/v3/${infuraId}`,
+      4: `https://rinkeby.infura.io/v3/${infuraId}`,
+    },
+  }),
+  new CoinbaseWalletConnector({
+    appName: "Vue Dapp",
+    jsonRpcUrl: `https://mainnet.infura.io/v3/${infuraId}`,
+  }),
+];
+
+const connectorsCreated = ref(false);
+
+onBeforeMount(async () => {
+  const safe = new SafeConnector();
+
+  try {
+    if (await safe.isSafeApp()) {
+      connectors = [safe];
+    }
+  } catch (err: any) {
+    console.error(err);
+  }
+
+  connectorsCreated.value = true;
+});
+
+const autoConnectErrorHandler = (err: any) => {
+  console.error(err);
+};
+
+const connectErrorHandler = (err: any) => {
+  console.error(err);
+};
+</script>
+
 <template>
+  <LayoutHeader></LayoutHeader>
   <div class="max-w-full w-[900px] m-auto">
     <NuxtPage />
   </div>
+  <vd-board
+    v-if="connectorsCreated"
+    :connectors="connectors"
+    dark
+    :autoConnectErrorHandler="autoConnectErrorHandler"
+    :connectErrorHandler="connectErrorHandler"
+  />
 </template>
---
+++ pages/connect.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Nuxt3 Vue-Dapp Starter</h1>
+
+    <p>Address: {{ address }}</p>
+    <p>Chain: {{ chainId }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useEthers } from "vue-dapp";
+
+const { address, chainId } = useEthers();
+</script>
