2d1fe05e7ddadaff0ef4c1f8dff50ed8ab5d8051
Franz Bendezu <franzant20@gmail.com>
2023-06-03 03:53:46 -0500

    fix: move app index for page

--- app.vue
+++ app.vue
@@ -1,17 +1,5 @@
-<script setup lang="ts">
-const isExtension =useIsExtension()
-</script>
 <template>
   <div class="max-w-full w-[900px] m-auto">
-    <NuxtErrorBoundary>
-      <SocialMediaPostGenerator />
-      <ChatWidget v-if="!isExtension"/>
-      <template #error="{ error }">
-        <div>
-          <p class="text-4xl pt-10">⚠️ A fatal error has occurred:</p>
-          <p>{{ error }}</p>
-        </div>
-      </template>
-    </NuxtErrorBoundary>
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const isExtension = useIsExtension();
+</script>
+<template>
+  <NuxtErrorBoundary>
+    <SocialMediaPostGenerator />
+    <ChatWidget v-if="!isExtension" />
+    <template #error="{ error }">
+      <div>
+        <p class="text-4xl pt-10">⚠️ A fatal error has occurred:</p>
+        <p>{{ error }}</p>
+      </div>
+    </template>
+  </NuxtErrorBoundary>
+</template>
