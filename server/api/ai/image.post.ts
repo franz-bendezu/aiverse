e21e64046b5dcd33965873dbaaaabdc713209b10
Franz Bendezu <franzant20@gmail.com>
2023-06-24 13:25:13 -0500

    fix: send redeable error

--- server/api/ai/image.post.ts
+++ server/api/ai/image.post.ts
@@ -12,35 +12,41 @@ export default defineEventHandler(async (event) => {
   });
   const openai = new OpenAIApi(configuration);
 
-  const { data: prompt } = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    temperature: 0,
-    messages: [
-      { role: "system", content: "You are a prompt enginer for DALL-E" },
-      {
-        role: "user",
-        content: `Proporcione 4 claves para la descripción de esta ilustración .
+  try {
+    const { data: prompt } = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+      messages: [
+        { role: "system", content: "You are a prompt enginer for DALL-E" },
+        {
+          role: "user",
+          content: `Proporcione 4 claves para la descripción de esta ilustración .
         ${url}`,
-      },
-    ],
-  });
+        },
+      ],
+    });
 
-  if (!prompt?.choices[0].message?.content) {
-    throw new Error("DALL-E prompt not generated");
+    if (!prompt?.choices[0].message?.content) {
+      throw new Error("DALL-E prompt not generated");
+    }
+    const dallePrompt = prompt?.choices[0].message?.content.trim();
+
+    const { data } = await openai.createImage({
+      prompt:
+        "Ilustración de estilo profesional, basada en las claves" + dallePrompt,
+    });
+    const imageURL = data.data[0].url;
+    if (!imageURL) throw new Error("Image not generated");
+    const res = await $fetch<Buffer>(imageURL, {
+      responseType: "arrayBuffer",
+    });
+    const base64String = Buffer.from(res).toString("base64");
+
+    return `data:image/jpeg;base64,${base64String}`;
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error),
+    };
   }
-  const dallePrompt = prompt?.choices[0].message?.content.trim();
-
-  const { data } = await openai.createImage({
-    prompt:
-      "Ilustración de estilo profesional, basada en las claves" +
-      dallePrompt,
-  });
-  const imageURL = data.data[0].url;
-  if (!imageURL) throw new Error("Image not generated");
-  const res = await $fetch<Buffer>(imageURL, {
-    responseType: "arrayBuffer",
-  });
-  const base64String = Buffer.from(res).toString("base64");
-
-  return `data:image/jpeg;base64,${base64String}`;
 });
