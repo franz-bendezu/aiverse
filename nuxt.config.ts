3044621244fa875d884c29f016c2977c224cea94
Franz Bendezu <franzant20@gmail.com>
2023-06-29 23:44:36 -0500

    feat: setup pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@pinia/nuxt"],
   runtimeConfig: {
     OPENAI_API_KEY: "",
     public: {
