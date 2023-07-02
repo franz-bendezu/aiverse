143c4179ab13eccd65a3396993cbf52a659aee71
Franz Bendezu <franzant20@gmail.com>
2023-07-02 17:02:40 -0500

    feat: change layout of app

--- pages/app.vue
+++ pages/app.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="flex flex-col items-center ">
-      <div class="d-tabs d-tabs-boxed">
-        <RouterLink
-          to="/app"
-          exact-active-class="d-tab-active"
-          exact
-          class="d-tab d-tab-lg d-tab-lifted"
-          >Inicio
-        </RouterLink>
-        <RouterLink
-          to="/app/generate"
-          active-class="d-tab-active"
-          class="d-tab d-tab-lg d-tab-lifted"
-          >Generador de imagenes
-        </RouterLink>
-        <RouterLink
-          to="/app/assets"
-          active-class="d-tab-active"
-          class="d-tab d-tab-lg d-tab-lifted"
-        >
-          Mi contenido
-        </RouterLink>
-      </div>
-    <NuxtPage></NuxtPage>
+  <div class="flex flex-col items-center">
+    <div class="d-tabs d-tabs-boxed">
+      <RouterLink
+        to="/app"
+        exact-active-class="d-tab-active"
+        exact
+        class="d-tab d-tab-lg d-tab-lifted"
+        >Inicio
+      </RouterLink>
+      <RouterLink
+        to="/app/generate"
+        active-class="d-tab-active"
+        class="d-tab d-tab-lg d-tab-lifted"
+        >Generador de imagenes
+      </RouterLink>
+      <RouterLink
+        to="/app/assets"
+        active-class="d-tab-active"
+        class="d-tab d-tab-lg d-tab-lifted"
+      >
+        Mi contenido
+      </RouterLink>
+    </div>
   </div>
+
+  <NuxtPage></NuxtPage>
 </template>
 
 <script></script>
