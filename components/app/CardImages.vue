7dd9a964e2315b5a8a6141459949dd8329ce4e4b
Franz Bendezu <franzant20@gmail.com>
2023-07-03 21:08:03 -0500

    feat: move button for protect

--- components/app/CardImages.vue
+++ components/app/CardImages.vue
@@ -6,46 +6,12 @@ defineProps({
 const emits = defineEmits(["regenerate", "protect"]);
 
 const { image, state, title, generate } = useImageAi();
-
-const gradients = [
-  {
-    start: "#00af99",
-    finish: "#90c73f",
-  },
-  {
-    start: "#b2a8cb",
-    finish: "#fe9173",
-  },
-  {
-    start: "#9afcf7",
-    finish: "#6e7cfa",
-  },
-  {
-    start: "#d54c7f",
-    finish: "#ed4758",
-  },
-];
 const onProtectImage = async () => {
   emits("protect");
 };
 </script>
 <template>
-  <AppCardGeneric :state="state">
-    <template #title>
-      <div class="my-1 flex w-full">
-        <div class="flex-grow">Images</div>
-        <div class="divider divider-horizontal"></div>
-        <div class="flex-grow grid place-items-right">
-          <button
-            @click="onProtectImage"
-            class="d-btn d-btn-primary px-2 py-2 rounded-full font-semibold text-white shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            <span v-if="loading" class="d-loading d-loading-spinner"></span>
-            Proteger Imagen
-          </button>
-        </div>
-      </div>
-    </template>
+  <AppCardGeneric compact :state="state">
     <template #body>
       <div class="md:flex gap-2 flex-wrap">
         <div class="mb-5 md:mb-0 flex-grow w-full">
@@ -58,8 +24,16 @@ const onProtectImage = async () => {
         </div>
       </div>
     </template>
-    <div v-if="url">
+    <div class="grid grid-cols-2 gap-5" v-if="url">
       <button class="d-btn d-btn-primary">Regenerar</button>
+      <button
+        v-if="url"
+        @click="onProtectImage"
+        class="d-btn d-btn-primary px-2 py-2 font-semibold text-white shadow-md hover:shadow-lg"
+      >
+        <span v-if="loading" class="d-loading d-loading-spinner"></span>
+        Proteger Imagen
+      </button>
     </div>
   </AppCardGeneric>
 </template>
