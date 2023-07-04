d3ef5c852b86563eef22879645479d6f99a74b68
Franz Bendezu <franzant20@gmail.com>
2023-07-03 21:06:55 -0500

    feat: add prompts for data

--- components/app/UrlForm.vue
+++ components/app/UrlForm.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import prompts from "~/assets/content/prompts.json";
+
 const props = defineProps<{
   url: string;
   temperature: number;
@@ -9,23 +11,47 @@ const form = reactive({
   url: props.url,
   temperature: props.temperature,
 });
+const getPrompt = () => {
+  const _propmts = prompts as { prompt: string }[];
+  const random = Math.floor(Math.random() * _propmts.length);
+  form.url = _propmts[random].prompt;
+};
 </script>
 <template>
   <form @submit.prevent="$emit('submit', form)" class="mb-10">
-    <div class="flex items-center mb-2">
+    <label class="text-sm font-bold my-3" for="description">
+      Ingresa una descripción
+    </label>
+    <div class="flex items-center my-3">
       <textarea
+        id="description"
         placeholder="Ingresa una descripción de tu contenido"
         v-model="form.url"
-        class="d-input d-input-bordered rounded-r-none w-full max-w-full mr-2  h-48"
+        class="d-input d-input-bordered rounded-r-none w-full max-w-full mr-2 h-48"
         type="text"
         rows="6"
-
       ></textarea>
     </div>
-    <button class="d-btn d-btn-primary px-2 py-2 w-full my-2">
-      <span v-if="loading" class="d-loading d-loading-spinner"></span>
-      Generar
-    </button>
+    <div class="grid grid-cols-2 gap-4">
+      <div>
+        <button
+          type="submit"
+          class="d-btn d-btn-primary px-2 py-2 w-full my-2 d-btn-sm"
+        >
+          <span v-if="loading" class="d-loading d-loading-spinner"></span>
+          Generar
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          @click="getPrompt"
+          class="d-btn d-btn-warning w-full my-2 d-btn-sm"
+        >
+          Dame una idea
+        </button>
+      </div>
+    </div>
     <AppTemperatureSelector v-model="form.temperature" />
   </form>
 </template>
